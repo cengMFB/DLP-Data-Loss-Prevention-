9cb76e3b55a9fb63825391a97c6ca10037b59f3e
Muhammed Furkan Beyaz <furkanve747@gmail.com>
2022-08-24 18:47:22 +0300

    DLP for endpoint device
    DLP(Data-Loss -Prevention for endpoint device)

---
+++ EndPoint-DLP/Scanner.py
@@ -0,0 +1,205 @@
+from watchdog.events import FileSystemEventHandler
+import os
+from PyPDF2 import PdfFileReader
+import mysql.connector
+import pandas as pd
+import re
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Dlp-747",
+    database="mydatabase"
+)
+
+path = os.path.join(os.path.join(os.environ['USERPROFILE']),'Desktop')
+os.chdir(path)
+
+class Handler(FileSystemEventHandler):
+    @staticmethod
+    def on_any_event(event):
+        # if event.is_directory:
+        #     return None
+        """print(
+            "[{}] noticed: [{}] on: [{}] ".format(
+                time.asctime(), event.event_type, event.src_path
+            )
+        )"""
+        mycursor = mydb.cursor()
+        mycursor.execute("DELETE FROM alerts")
+        path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        path2 = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')
+        #path3 = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Documents')
+        fileCheck(path)
+        fileCheck(path2)
+        #fileCheck(path3)
+
+def fileCheck(path):
+
+    try:
+        os.chdir(path)
+        for file in os.listdir():
+
+            if file.endswith(".xlsx"):
+                file_path = f"{path}\{file}"
+
+                df = pd.read_excel(file_path, dtype="string").to_string()
+                if re.search(r'[\w\.-]+@[\w\.-]+', df):
+                    mycursor = mydb.cursor()
+                    sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                    val = (file_path, "E-Mail Credential Found")
+                    mycursor.execute(sql, val)
+
+                    mydb.commit()
+
+                    print(mycursor.rowcount, "record inserted.")
+                    print(file_path + " credential information founded ")
+                if re.search(r'((\d)-?(?!(-?\2){3})){16}', df):
+                    pass
+
+            if file.endswith(".pdf"):
+                file_path = f"{path}\{file}"
+                pdf = PdfFileReader(file_path)
+                print(file_path)
+                for page_num in range(pdf.numPages):
+
+                    pageObj = pdf.getPage(page_num)
+                    txt = pageObj.extractText()
+                    if re.search(r'((\d)-?(?!(-?\2){3})){16}', txt):
+                        mycursor = mydb.cursor()
+
+                        sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                        val = (file_path, "Credit Card Credential Found")
+                        mycursor.execute(sql, val)
+
+                        mydb.commit()
+
+                        print(mycursor.rowcount, "record inserted.")
+                        print(file_path + " credential information founded ")
+
+                    if re.search(r'[\w\.-]+@[\w\.-]+', txt):
+                        print(file_path + " credential information founded ")
+                        mycursor = mydb.cursor()
+
+                        sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                        val = (file_path, "E-Mail Credential")
+                        mycursor.execute(sql, val)
+
+                        print(mycursor.rowcount, "record inserted.")
+                        mydb.commit()
+
+                        print(mycursor.rowcount, "record inserted.")
+                        print("mail found")
+
+            if file.endswith(".txt"):
+
+                file_path = f"{path}\{file}"
+                print(file_path)
+                with open(file_path, 'r') as f:
+                    lines = f.read()
+                    if re.search(r'((\d)-?(?!(-?\2){3})){16}', lines):
+                        mycursor = mydb.cursor()
+
+                        sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                        val = (file_path, "Credit Card Credential Found")
+                        mycursor.execute(sql, val)
+
+                        mydb.commit()
+
+                        print(mycursor.rowcount, "record inserted.")
+                        print(file_path + " credential information founded ")
+
+                    if re.search(r'[\w\.-]+@[\w\.-]+', lines):
+                        mycursor = mydb.cursor()
+
+                        sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                        val = (file_path, "E-Mail Credential")
+                        mycursor.execute(sql, val)
+
+                        mydb.commit()
+
+                        print(mycursor.rowcount, "record inserted.")
+                        print(file_path + " credential information founded ")
+
+                        print("mail found")
+
+            else:
+                fileCheck2(path, file)
+    except:
+        print("---------------DOSYA TASINMA HATASI-----------------")
+
+
+
+def fileCheck2(path, file):
+
+        dirPath = f"{path}\{file}"
+        if os.path.isdir(dirPath):
+
+            os.chdir(dirPath)
+
+            for file in os.listdir():
+
+                if file.endswith(".pdf"):
+                    file_path = f"{dirPath}\{file}"
+                    pdf = PdfFileReader(file_path)
+                    for page_num in range(pdf.numPages):
+
+                        pageObj = pdf.getPage(page_num)
+                        txt = pageObj.extractText()
+                        if re.search(r'((\d)-?(?!(-?\2){3})){16}', txt):
+                            mycursor = mydb.cursor()
+
+                            sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                            val = (file_path, "Credit Card Credential Found")
+                            mycursor.execute(sql, val)
+
+                            mydb.commit()
+
+                            print(mycursor.rowcount, "record inserted.")
+                            print(file_path + " credential information founded ")
+
+                        if re.search(r'[\w\.-]+@[\w\.-]+', txt):
+                            print(file_path + " credential information founded ")
+                            mycursor = mydb.cursor()
+
+                            sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                            val = (file_path, "E-Mail Credential Found")
+                            mycursor.execute(sql, val)
+
+                            mydb.commit()
+
+                            print(mycursor.rowcount, "record inserted.")
+                            print("mail found")
+
+                if file.endswith(".txt"):
+                    file_path = f"{dirPath}\{file}"
+                    with open(file_path, 'r') as f:
+                        lines = f.read()
+                        if re.search(r'((\d)-?(?!(-?\2){3})){16}', lines):
+                            mycursor = mydb.cursor()
+
+                            sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                            val = (file_path, "Credit Card Credential Found")
+                            mycursor.execute(sql, val)
+
+                            mydb.commit()
+
+                            print(mycursor.rowcount, "record inserted.")
+                            print(file_path + " credential information founded ")
+
+                        if re.search(r'[\w\.-]+@[\w\.-]+', lines):
+                            mycursor = mydb.cursor()
+
+                            sql = "INSERT INTO alerts (filePath, eventType) VALUES (%s, %s)"
+                            val = (file_path, "E-Mail Credential Found")
+                            mycursor.execute(sql, val)
+
+                            mydb.commit()
+
+                            print(mycursor.rowcount, "record inserted.")
+                            print(file_path + " credential information founded ")
+
+                else:
+                    return fileCheck2(dirPath, file)
+
+
---
+++ EndPoint-DLP/main.py
@@ -0,0 +1,46 @@
+import time
+from watchdog.observers import Observer
+import ctypes, sys
+import Scanner
+import os
+
+WatcherPath = os.path.join(os.path.join(os.environ['USERPROFILE']),'Desktop')
+path2 = os.path.join(os.path.join(os.environ['USERPROFILE']),'Downloads')
+class Watcher:
+    def __init__(self, path):
+        self.observer = Observer()
+        self.path = path
+
+    def run(self):
+
+        event_handler = Scanner.Handler()
+        self.observer.schedule(event_handler, self.path, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except:
+            self.observer.stop()
+            print("Error")
+
+        self.observer.join()
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
+if __name__ == "__main__":
+    Scanner.Handler()
+    if is_admin():
+
+        w = Watcher(WatcherPath)
+
+        w.run()
+
+
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
